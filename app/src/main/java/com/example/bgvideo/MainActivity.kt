7a4f87f06dd394b6115e9c1bb9de0d5c6e8d4acc
shutov.vu <v.shutov2010@gmail.com>
2023-03-17 16:36:20 +0300

    New code added

--- app/src/main/java/com/example/bgvideo/MainActivity.kt
+++ app/src/main/java/com/example/bgvideo/MainActivity.kt
@@ -9,29 +9,4 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
     }
 
-    override fun onStart() {
-        super.onStart()
-    }
-
-    override fun onResume() {
-        super.onResume()
-    }
-
-    override fun onPause() {
-        super.onPause()
-    }
-
-    override fun onStop() {
-        super.onStop()
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-    }
-
-    override fun onRestart() {
-        super.onRestart()
-    }
-
-
 }
