1c0e8eb405a81fba4733feb5a5609ed6b52aba3c
shutov.vu <v.shutov2010@gmail.com>
2023-03-21 16:02:47 +0300

    New code added

--- app/src/main/java/com/example/bgvideo/MainActivity.kt
+++ app/src/main/java/com/example/bgvideo/MainActivity.kt
@@ -4,19 +4,23 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import com.example.bgvideo.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    val buttonLogin = findViewById<Button>(R.id.Button_login)
-
+    lateinit var bindingClass : ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
 
     fun onClickButtonLogin(view : View){
-        
+        //buttonLogin = findViewById<Button>(R.id.Button_login)
+        bindingClass.ButtonLogin.setOnClickListener{
+            setContentView(R.layout.activity_main_map)
+        }
     }
 
 }
