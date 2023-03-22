24c693bc667bfa20d39eca6b08ca8b6412f950ae
shutov.vu <v.shutov2010@gmail.com>
2023-03-22 17:33:00 +0300

    New code added

--- app/src/main/java/com/example/bgvideo/MainActivity_map.kt
+++ app/src/main/java/com/example/bgvideo/MainActivity_map.kt
@@ -2,6 +2,7 @@ package com.example.bgvideo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.bgvideo.databinding.ActivityMainMapBinding
 import com.yandex.mapkit.Animation
 import com.yandex.mapkit.MapKitFactory
 import com.yandex.mapkit.geometry.Point
@@ -18,8 +19,8 @@ class MainActivity_map : AppCompatActivity() {
         MapKitFactory.initialize(this)
         setContentView(R.layout.activity_main_map)
         mapView = findViewById(R.id.ya_mapview)
-        mapView.map.move(CameraPosition(Point(64.546068, 40.551780), 13.0f, 0.0f, 0.0f),
-        Animation(Animation.Type.SMOOTH, 2f), null)
+        mapView.map.move(CameraPosition(Point(64.546068, 40.551780), 13f, 0.0f, 0.0f),
+        Animation(Animation.Type.SMOOTH, 1.5f), null)
     }
 
     override fun onStop() {
